55709658bd4e08296cb6c000acf0324bc915ff9e
vudaluzusa <49219138+vudaluzusa@users.noreply.github.com>
2019-04-03 02:40:23 +0000

    Create app-chooser.py

---
+++ app-chooser.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import sys, os, os.path
+filename = sys.argv[1];
+extension = os.path.splitext(filename)[1];
+
+if extension == "txt":
+  os.system("gedit " + filename);
+
+if extension == "mp4":
+  os.system("vlc " + filename);
+  
+if extension == "html":
+  os.system("opera " + filename);
+  
+if extension == ".py":
+  os.system("python " + filename);
+  
+if extension == ".sh":
+  os.system("bash " + filename);
+  
+if extension == ".exe":
+  os.system("wine " + filename);
+  
+exit()
